43c66e045f92973d280619c9ff731595fa003e8e
Razvan Meriniuc <razvan_meriniuc@yahoo.com>
2020-04-02 22:59:17 +0300

    Add files via upload

---
+++ scripts/align_embeddings_to_snli_vocab.py
@@ -0,0 +1,46 @@
+import pickle
+import numpy as np
+import matplotlib.pyplot as plt
+
+words = pickle.load(open('id_to_word', 'rb'))
+words_set = set(words)
+
+poincare_words = {}
+in_em = []
+out_em = []
+with open('100D_vanilla_GloVe.txt', 'r') as f:
+    f.readline()
+    l = f.readline()
+    c = 0
+    while l:
+        l = l.strip().split(' ')
+        c += 1
+
+        if l[0] in words_set:
+            poincare_words[l[0]] = list(map(lambda x: float(x), l[1 :]))
+        
+        l = f.readline()
+
+embedds = []
+count_w = 0
+count_t = 0
+for w in words:
+    count_t += 1
+    if w in poincare_words:
+        count_w += 1
+        embedds.append(poincare_words[w])
+        in_em.append(poincare_words[w])
+    else:
+        x = np.random.uniform(-0.001, 0.001, 100)
+        embedds.append(np.random.uniform(-0.001, 0.001, 100))
+        out_em.append(x)
+
+print(count_w / count_t)
+print(count_w)
+print(count_t)
+in_em = np.array(in_em)
+out_em = np.array(out_em)
+plt.scatter(in_em[:, 0], in_em[:, 1], c = 'r')
+plt.scatter(out_em[:, 0], out_em[:, 1], c = 'b')
+plt.show()
+pickle.dump(embedds, open('100d_snli_embedds_vanilla_glove', 'wb'))
